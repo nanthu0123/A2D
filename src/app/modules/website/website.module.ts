277dbc635d4c7bf11bdae3e9a8c8de05eb0d7400
nanthagopal <nanthu0123@gmail.com>
2022-06-22 10:28:29 +0530

    skeleton setup

--- src/app/modules/website/website.module.ts
+++ src/app/modules/website/website.module.ts
@@ -6,6 +6,9 @@ import { WebsiteRoutingModule } from './website-routing.module';
 import { FooterComponent, HeaderComponent } from './layouts';
 import { LandingComponent, LoginComponent } from './pages';
 import { NavbarComponent, SocialMediaComponent } from './components';
+import { BannerComponent } from './components/banner/banner.component';
+import { TestimonialComponent } from './components/testimonial/testimonial.component';
+import { HomeMainComponent } from './components/home-main/home-main.component';
 
 
 @NgModule({
@@ -15,7 +18,10 @@ import { NavbarComponent, SocialMediaComponent } from './components';
     NavbarComponent,
     SocialMediaComponent,
     LandingComponent,
-    LoginComponent
+    LoginComponent,
+    BannerComponent,
+    TestimonialComponent,
+    HomeMainComponent
   ],
   imports: [
     CommonModule,
